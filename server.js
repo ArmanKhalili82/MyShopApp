217ba15c4ab7d9c70ac8beb6ace2f18a1ef33032
ArmanKhalili82 <hykhalili@yahoo.com>
2023-08-05 23:06:46 +0200

    nodemon problem

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express')
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send(<h1>Welcome To Our Shop App</h1>);
+    res.send("<h1>Welcome To Our Shop App</h1>");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
-})
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
+});
